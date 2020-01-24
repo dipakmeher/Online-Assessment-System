c4609852026bbb2fdaf6cb947c7cf4af01aa0a02
dipakmeher <dipakmeher54@gmail.com>
2020-01-24 14:49:22 +0530

    Tried fixing radio button solution

--- pages/index.vue
+++ pages/index.vue
@@ -12,10 +12,14 @@
               <p class="display-1 text--primary">
                 {{Questions.Question}}
               </p>          
-              <v-radio-group :mandatory="false" v-model="picked">
-                <v-radio v-for="choice in Questions.Choices" :key="choice" v-bind:label="choice" v-bind:value="choice" color="white"></v-radio>
-              </v-radio-group>
-              <span>Picked: {{ picked  }}</span>
+              <div class="radio radio-primary"> 
+                <label v-for="choice in Questions.Choices" v-bind:key="choice['c1']">
+                  <input type="radio" :name="Questions.Question" id="radio" :value="choice" v-model="selectedrole"/>
+                  {{choice}}
+                  <br/>
+                </label>                          
+              </div>
+              <!-- <span>Picked: {{ picked  }}</span> -->
             </v-card-text>
             <v-btn depressed small color="primary" class="ma-5" @click="Firestoreupdate">Submit</v-btn>
           </v-card>
@@ -25,17 +29,41 @@
       <v-col cols="3" class="col-3 grey lighten-3"></v-col>
     </v-row>
   </div>
+  <!-- <v-radio-group :mandatory="false" v-model="picked">
+                <v-radio v-for="choice in Questions.Choices" :key="choice['c1']" v-bind:label="choice" v-bind:value="choice" color="white" v-on:click="addRadio(Questions)"></v-radio>
+              </v-radio-group>
+                    <input type="radio" :name= "Questions" id="radio" :value= choice v-on:click="update" v-model="selectedrole"/>
+ -->
 </template>
 
 <script>
 //import firebase from '../plugins/firebase'
 import db from '../plugins/firebase'
+var dict = new Object();
+var dict= {};
+
+
 export default {
   data() {
     return {
       projects: [],
-      answer:[],
-      picked:""
+      dict:{},
+      selectedrole:""
+    }
+  },
+  methods:{
+    Firestoreupdate(){
+      db.collection('Assessment').doc('Book-Distribution').update({
+          "Question1":{
+            "Answer": this.picked,
+            "Choices": this.projects[0].Question1.Choices,
+            "Question":this.projects[0].Question1.Question
+        }
+      });
+    },
+    addRadio(Questions){
+      dict[Questions]=this.picked;
+      console.log(dict[Questions]);
     }
   },
   created() {
@@ -50,23 +78,7 @@ export default {
         }  
       })
     });
-  },
-  methods:{
-    Firestoreupdate(){
-      console.log(this.projects);
-      console.log(this.projects[0].Question1.Question);
-
-        db.collection('Assessment').doc('Book-Distribution').update({
-          "Question1":{
-            "Answer": this.picked,
-            "Choices": this.projects[0].Question1.Choices,
-            "Question":this.projects[0].Question1.Question
-        }
-      });
-    },
-    addRadio(){
-      console.log("Hare Krishna");
-    }
+    console.log(this.projects);
   }
 }
  

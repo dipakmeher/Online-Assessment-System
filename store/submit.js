7cbd1bb050829654b7069a89894087af6d9808c9
dipakmeher <dipakmeher54@gmail.com>
2020-02-28 20:55:53 +0530

    Added Submit page

---
+++ store/submit.js
@@ -0,0 +1,36 @@
+import db from '../plugins/firebase'
+
+export const state = () => ({
+    CorrectAnswer:[]
+})
+
+
+export const getters = {
+    getCorrectAns (state) {
+      return state.CorrectAnswer
+    }
+}
+
+
+export const mutations={
+    add(state,title){
+        state.CorrectAnswer.push(title)
+      }
+}
+
+export const actions={
+    // Fetch Categories
+    fetchScore({ commit }) {
+        db.collection("Assessment").doc("Correct-Answers").get().then(function(doc) {
+            if (doc.exists) {
+                commit("add",doc.data());
+                //console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+    }
+  }
